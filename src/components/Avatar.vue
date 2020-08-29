f169df6f9b44b89da6dd19a5c13ac0df31e6a8b5
Krzysztof Kaczyński <34687242+kaczor6418@users.noreply.github.com>
2020-08-29 19:08:37 +0200

    feat(message-container): add message container
    * feat(time-converter): add time converter
    
    * feat(avatar-component): add avatar component
    
    * feat(single-comment): add single comment component
    
    * feat(divider-component): add divider component
    
    * feat(comments-container): add comments container component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,25 @@
+<template>
+    <object :data="avatarUrl">
+        <img :src="defaultAvatarUrl" alt="Default avatar" />
+    </object>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Avatar extends Vue {
+    @Prop({ required: true }) readonly avatarUrl!: string;
+    @Prop({ default: 'https://brainstormcursos.com.br/wp-content/uploads/2016/01/default-avatar-128x128.jpg' })
+    readonly defaultAvatarUrl!: string;
+}
+</script>
+
+<style lang="scss">
+object,
+img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+}
+</style>
---
+++ src/components/Divider.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <hr />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Divider extends Vue {}
+</script>
+
+<style lang="scss">
+hr {
+    margin: 0;
+    border-color: #e8e8e8;
+}
+</style>
