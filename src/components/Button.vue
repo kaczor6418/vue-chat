5dd0d3f996a041a5e6917ce8d82e5ebe1c905b30
kaczor6418 <k.kaczynski97@gmail.com>
2020-08-30 11:31:02 +0200

    feat(button): add base button component

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button v-on="$listeners"><slot></slot></button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class BaseButton extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+button {
+    border: 0;
+    padding: 0;
+    outline: none;
+    background-color: transparent;
+    cursor: pointer;
+    color: #0abaf4;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
